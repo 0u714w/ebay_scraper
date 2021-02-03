871151bed91252829e51f00e4eb7cac75aeec47b
Douglass Enas <douglass@Douglasss-MBP.localdomain>
2021-02-03 13:28:24 -0500

    csv download plus date sold

--- multiply/views.py
+++ multiply/views.py
@@ -20,6 +20,7 @@ def create_csv_sold(keyword):
     average_price = []
     prices_stripped = []
     outliers = []
+    sold_dates = []
     url = "https://www.ebay.com/sch/i.html?_from=R40&_nkw={}&_in_kw=1&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&_samilow=&_samihi=&_sadis=15&_stpos=46201&_sargn=-1%26saslc%3D1&_salic=1&_sop=12&_dmd=1&_ipg=50&LH_Complete=1&_fosrp=1".format(keyword)
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -37,6 +38,10 @@ def create_csv_sold(keyword):
                 prod_price = str(price.find(text=True, recursive=False))
                 match = re.search(r'\b\$?[\d,.]+\b', str(prod_price))
                 match_2 = re.search(r'\b\$?[\d,.]+\b', str(price_to))
+                sold_date = listing.find('span', attrs={'class': 'tme'})
+                sold_date = str(sold_date)
+                sold_dates.append(sold_date[25:-14])
+                
                 if match_2 == 'None':
                     pass
                 elif match_2:
@@ -53,10 +58,11 @@ def create_csv_sold(keyword):
 
     if len(average_price) < len(outliers):
         average_price.append(helpers.generate_average(prices_stripped))
+    
 
 
     # username = getpass.getuser()
-    chart = pd.DataFrame({"Name": item_name, "Prices": prices, "Average Price": helpers.remove_outlier_from_average(outliers, average_price), "Outliers": outliers})
+    chart = pd.DataFrame({"Name": item_name, "Prices": prices, "Average Price": helpers.remove_outlier_from_average(outliers, average_price), "Outliers": outliers, 'Date Sold': sold_dates})
     # chart.to_csv(r'/Users/{}/Desktop/{}_sold.csv'.format(username, keyword), index=False)
     return chart
     # r = requests.post('https://ops.multiplytechnology.com/ops4/api/scraper', data = {'chart': chart.to_json()})
@@ -94,9 +100,7 @@ def create_csv_active(keyword):
     if len(average_price) < len(outliers):
         average_price.append(helpers.generate_average(prices_stripped))
 
-    username = getpass.getuser()
     chart = pd.DataFrame({"Name": item_name, "Prices": prices, "Average Price": helpers.remove_outlier_from_average(outliers, average_price), "Outliers": outliers})
-    # chart.to_csv(r'/Users/{}/Desktop/{}_active.csv'.format(username, keyword), index=False)
     return chart
 
 @csrf_exempt
@@ -115,12 +119,19 @@ def homepage(request):
 
             elif data['download_csv_sold'] is True:
                 chart = create_csv_sold(data['search']).to_html()
+                username = getpass.getuser()
+
+                create_csv_sold(data['search']).to_csv(r'/Users/{}/Desktop/{}_sold.csv'.format(username, data['search']), index=False)
                 if data.get('response_type', 'Not Found') == 'json': 
                     return JsonResponse({'url': sold_url, 'chart' : chart.to_json()}, safe=False)
                 
                 
             elif data['download_csv_active'] is True:
                 chart = create_csv_active(data['search']).to_html()
+                username = getpass.getuser()
+
+                create_csv_active(data['search']).to_csv(r'/Users/{}/Desktop/{}_active.csv'.format(username, data['search']), index=False)
+
                 if data.get('response_type', 'Not Found') == 'json': 
                     return JsonResponse({'url': active_url, 'chart' : chart.to_json()}, safe=False)
                 
